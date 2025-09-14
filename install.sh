616ec7fc61d64e60bc4165e8c0a86fdf6142967f
Aiweb22 <thanujabandara67@gmail.com>
2025-09-14 17:16:05 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,28 +18,36 @@ installTheme(){
 
     echo -e "${GREEN}Installing ${YELLOW}sudo${GREEN} if not installed${RESET}"
     apt install sudo -y > /dev/null 2>&1
+
     cd /var/www/ > /dev/null 2>&1
     echo -e "${GREEN}Unpack the themebackup...${RESET}"
-    tar -cvf topatheam.tar.gz pterodactyl > /dev/null 2>&1
+    tar -cvf Pterodactyl_Nightcore_Themebackup.tar.gz pterodactyl > /dev/null 2>&1
+
     echo -e "${GREEN}Installing theme... ${RESET}"
     cd /var/www/pterodactyl > /dev/null 2>&1
+
     echo -e "${GREEN}Removing old theme if exist${RESET}"
     rm -r topatheam > /dev/null 2>&1
+
     echo -e "${GREEN}Download the Theme${RESET}"
     git clone https://github.com/Aiweb22/topatheam.git > /dev/null 2>&1
+
     cd topatheam/main > /dev/null 2>&1
+
     echo -e "${GREEN}Removing old theme resources if exist${RESET}"
     rm /var/www/pterodactyl/resources/scripts/Pterodactyl_Nightcore_Theme.css > /dev/null 2>&1
     rm /var/www/pterodactyl/resources/scripts/index.tsx > /dev/null 2>&1
+
     echo -e "${GREEN}Moving the new theme files to directory${RESET}"
     mv index.tsx /var/www/pterodactyl/resources/scripts/index.tsx > /dev/null 2>&1
     mv Pterodactyl_Nightcore_Theme.css /var/www/pterodactyl/resources/scripts/Pterodactyl_Nightcore_Theme.css > /dev/null 2>&1
+
     cd /var/www/pterodactyl > /dev/null 2>&1
-    
+
     curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash - > /dev/null 2>&1
     apt update -y > /dev/null 2>&1
     apt install nodejs -y > /dev/null 2>&1
-    
+
     NODE_VERSION=$(node -v)
     REQUIRED_VERSION="v16.20.2"
     if [ "$NODE_VERSION" != "$REQUIRED_VERSION" ]; then
@@ -77,7 +85,7 @@ installThemeQuestion(){
 }
 
 repair(){
-    bash <(curl https://raw.githubusercontent.com/NoPro200/Pterodactyl_Nightcore_Theme/main/repair.sh)
+    bash <(curl https://raw.githubusercontent.com/Aiweb22/topatheam/main/repair.sh)
 }
 
 restoreBackUp(){
@@ -101,19 +109,15 @@ echo "[3] Repair panel (use if you have an error in the theme installation)"
 echo "[4] Exit"
 
 read -p "Please enter a number: " choice
-if [ $choice == "1" ]
-    then
+if [ "$choice" == "1" ]; then
     installThemeQuestion
 fi
-if [ $choice == "2" ]
-    then
+if [ "$choice" == "2" ]; then
     restoreBackUp
 fi
-if [ $choice == "3" ]
-    then
+if [ "$choice" == "3" ]; then
     repair
 fi
-if [ $choice == "4" ]
-    then
+if [ "$choice" == "4" ]; then
     exit
 fi
